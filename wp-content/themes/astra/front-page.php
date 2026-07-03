d61bdb0234866d597db8887a203f197c5a81b13d
Muhammad Imran <mimran@abitcons.com>
2026-07-03 08:28:51 +0400

    TASK-2026-02176: Restore abit.ai availability

--- wp-content/themes/astra/front-page.php
+++ wp-content/themes/astra/front-page.php
@@ -11,23 +11,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
-if ( astra_page_layout() === 'left-sidebar' ) {
+if ( function_exists( 'astra_page_layout' ) && astra_page_layout() === 'left-sidebar' ) {
 	get_sidebar();
 }
 ?>
 
-<div id="primary" <?php astra_primary_class(); ?>>
+<div id="primary" <?php function_exists( 'astra_primary_class' ) ? astra_primary_class() : post_class( 'content-area primary' ); ?>>
 
-	<?php astra_primary_content_top(); ?>
+	<?php
+	if ( function_exists( 'astra_primary_content_top' ) ) {
+		astra_primary_content_top();
+	}
 
-	<?php astra_content_page_loop(); ?>
+	if ( function_exists( 'astra_content_page_loop' ) ) {
+		astra_content_page_loop();
+	} elseif ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
 
-	<?php astra_primary_content_bottom(); ?>
+			if ( locate_template( 'template-parts/content-page.php' ) ) {
+				get_template_part( 'template-parts/content', 'page' );
+			} else {
+				the_content();
+			}
+		}
+	}
+
+	if ( function_exists( 'astra_primary_content_bottom' ) ) {
+		astra_primary_content_bottom();
+	}
+	?>
 
 </div><!-- #primary -->
 
 <?php
-if ( astra_page_layout() === 'right-sidebar' ) {
+if ( function_exists( 'astra_page_layout' ) && astra_page_layout() === 'right-sidebar' ) {
 	get_sidebar();
 }
 
